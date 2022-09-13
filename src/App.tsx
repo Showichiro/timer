3875ad88783bf421421b9a14b281da5aa762170b
uchihori <show2305@gmail.com>
2022-09-14 02:38:50 +0900

    footer修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,32 @@
 import AddButton from "./components/AddButton";
 import { Timer } from "./components/Timer";
 import { useTimerList } from "./hooks/useTimerList";
+import Footer from "./shared/Footer";
 
 function App() {
   const { timerList, removeTimer, addNewTimer, updateTimer } = useTimerList();
   return (
-    <div className="p-2">
-      <h1 className="font-black text-3xl">Timer</h1>
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 pt-2">
-        {timerList.map((timer) => (
-          <Timer
-            key={timer.id}
-            defaultTimerValue={timer.timerValue}
-            defaultTitle={timer.title}
-            onClickDelete={() => removeTimer(timer.id)}
-            onEditTimerValue={(timerValue) =>
-              updateTimer({ ...timer, timerValue })
-            }
-            onEditTitle={(title) => updateTimer({ ...timer, title })}
-          />
-        ))}
-      </div>
-      <AddButton onClick={addNewTimer} />
-    </div>
+    <>
+      <main className="p-2">
+        <h1 className="font-black text-3xl">Timer</h1>
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-3 pt-2">
+          {timerList.map((timer) => (
+            <Timer
+              key={timer.id}
+              defaultTimerValue={timer.timerValue}
+              defaultTitle={timer.title}
+              onClickDelete={() => removeTimer(timer.id)}
+              onEditTimerValue={(timerValue) =>
+                updateTimer({ ...timer, timerValue })
+              }
+              onEditTitle={(title) => updateTimer({ ...timer, title })}
+            />
+          ))}
+        </div>
+        <AddButton onClick={addNewTimer} />
+      </main>
+      <Footer />
+    </>
   );
 }
 
