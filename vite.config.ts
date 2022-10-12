3159ee33d08e955bb90a71aea5e2c68b6dae9977
uchihori <show2305@gmail.com>
2022-10-12 19:48:25 +0900

    モジュールサイズ調整

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/timer/",
+  build: {
+    rollupOptions: {
+      plugins: [visualizer()],
+      output: {
+        manualChunks: {
+          "vendor": ["react", "react-dom"]
+        }
+      }
+    }
+  }
 });
