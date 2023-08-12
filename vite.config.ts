77992a27c91157c42769958292f1d6b516049cad
uchihori <show2305@gmail.com>
2023-08-12 19:10:41 +0900

    add: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(
+    {
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "Timer",
+        description: "Countdown timer application. You can set as many timers as you want. The timers are stored, so they are not lost even if you close the browser.",
+        short_name: "Timer",
+        lang: "ja",
+        start_url: "index.html",
+        icons: [
+          {
+            src: "icon.png",
+            sizes: "512x512",
+            purpose: "any"
+          },
+          {
+            src: "icon-mini.png",
+            sizes: "192x192",
+            purpose: "any"
+          }
+        ]
+      }
+    }
+  )],
   base: "/timer/",
   build: {
     rollupOptions: {
