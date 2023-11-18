6208c683fd6d323633a6123dbef40d355b8e4fe4
uchihori <show2305@gmail.com>
2023-11-19 03:01:37 +0900

    test: add hooks tests

---
+++ src/hooks/useIsFirst.test.ts
@@ -0,0 +1,39 @@
+import { useIsFirst } from "./useIsFirst";
+import { renderHook } from "../test/test-utils";
+
+describe("useIsFirst", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  const { isLocalStorageAvailable } = vi.hoisted(() => ({
+    isLocalStorageAvailable: vi.fn(() => true),
+  }));
+
+  vi.mock("../utils/localStorageUtil", () => {
+    return {
+      isLocalStorageAvailable,
+    };
+  });
+
+  it("should return true when localStorage is not available", () => {
+    isLocalStorageAvailable.mockReturnValue(false);
+    expect(renderHook(() => useIsFirst()).result.current).toBe(true);
+  });
+
+  it("should return true when localStorage is available and localStorage value (isFirst) is null", () => {
+    isLocalStorageAvailable.mockReturnValue(true);
+    vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+    const setItemSpy = vi.spyOn(Storage.prototype, "setItem");
+    expect(renderHook(() => useIsFirst()).result.current).toBe(true);
+    expect(setItemSpy).toHaveBeenCalledWith("isFirst", "false");
+  });
+
+  it("should return false when localStorage is available and localStorage value (isFirst) is 'true'", () => {
+    isLocalStorageAvailable.mockReturnValue(true);
+    vi.spyOn(Storage.prototype, "getItem").mockReturnValue("false");
+    const setItemSpy = vi.spyOn(Storage.prototype, "setItem");
+    expect(renderHook(() => useIsFirst()).result.current).toBe(false);
+    expect(setItemSpy).toHaveBeenCalledWith("isFirst", "false");
+  });
+});
---
+++ src/hooks/useTimerList.test.ts
@@ -0,0 +1,340 @@
+import { TimerType, initializer, reducer, useTimerList } from "./useTimerList";
+import { renderHook } from "../test/test-utils";
+
+const { getTimerFromUrl } = vi.hoisted(() => {
+  return {
+    getTimerFromUrl: vi.fn(),
+  };
+});
+
+vi.mock("../utils/urlUtil", () => {
+  return {
+    getTimerFromUrl,
+  };
+});
+
+describe("userTimerList", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  const state = [
+    {
+      id: "1",
+      type: "stopwatch" as TimerType,
+      timerValue: {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      },
+      title: "timer",
+    },
+  ];
+
+  describe("reducer", () => {
+    it("should add new timer state when called with addNewTImer", () => {
+      expect(
+        reducer(state, {
+          type: "addNewTimer",
+          args: { id: "2", type: "countdown" },
+        }),
+      ).toEqual([
+        {
+          id: "1",
+          type: "stopwatch" as TimerType,
+          timerValue: {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          },
+          title: "timer",
+        },
+        {
+          id: "2",
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+      ]);
+    });
+
+    it("should update timer state when called with updateTimer", () => {
+      expect(
+        reducer(state, {
+          type: "updateTimer",
+          args: {
+            id: "1",
+            title: "new title",
+            timerValue: { hours: 0, minutes: 0, seconds: 0 },
+            type: "stopwatch" as TimerType,
+          },
+        }),
+      ).toEqual([
+        {
+          id: "1",
+          type: "stopwatch" as TimerType,
+          timerValue: {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          },
+          title: "new title",
+        },
+      ]);
+    });
+
+    it("should remove timer state when called with removeTimer", () => {
+      expect(
+        reducer(state, {
+          type: "removeTimer",
+          args: { id: "1" },
+        }),
+      ).toEqual([]);
+    });
+  });
+
+  describe("initializer", () => {
+    it("shoud return initial value when localstorage does not have savedList", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      expect(initializer()).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+      ]);
+    });
+
+    it("should return savedList when localstorage has savedList", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(
+        JSON.stringify([
+          {
+            id: "1",
+            timerValue: {
+              hours: 0,
+              minutes: 0,
+              seconds: 0,
+            },
+            title: "timer1",
+            type: "countdown",
+          },
+          {
+            id: "2",
+            timerValue: {
+              hours: 1,
+              minutes: 2,
+              seconds: 3,
+            },
+            title: "timer2",
+            type: "stopwatch",
+          },
+        ]),
+      );
+      expect(initializer()).toEqual([
+        {
+          id: "1",
+          timerValue: {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          },
+          title: "timer1",
+          type: "countdown",
+        },
+        {
+          id: "2",
+          timerValue: {
+            hours: 1,
+            minutes: 2,
+            seconds: 3,
+          },
+          title: "timer2",
+          type: "stopwatch",
+        },
+      ]);
+    });
+
+    it("should return urlValue when getTimerFromUrl returns urlValue", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(
+        JSON.stringify([
+          {
+            id: "1",
+            timerValue: {
+              hours: 0,
+              minutes: 0,
+              seconds: 0,
+            },
+            title: "timer1",
+            type: "countdown",
+          },
+          {
+            id: "2",
+            timerValue: {
+              hours: 1,
+              minutes: 2,
+              seconds: 3,
+            },
+            title: "timer2",
+            type: "stopwatch",
+          },
+        ]),
+      );
+      getTimerFromUrl.mockReturnValue({
+        hours: 5,
+        minutes: 6,
+        seconds: 7,
+      });
+      expect(initializer()).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: {
+            hours: 5,
+            minutes: 6,
+            seconds: 7,
+          },
+          title: undefined,
+          type: "countdown",
+        },
+        {
+          id: "1",
+          timerValue: {
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+          },
+          title: "timer1",
+          type: "countdown",
+        },
+        {
+          id: "2",
+          timerValue: {
+            hours: 1,
+            minutes: 2,
+            seconds: 3,
+          },
+          title: "timer2",
+          type: "stopwatch",
+        },
+      ]);
+    });
+  });
+
+  describe("hooks", () => {
+    it("returns timerList", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      expect(renderHook(() => useTimerList()).result.current.timerList).toEqual(
+        [
+          {
+            id: expect.any(String),
+            timerValue: undefined,
+            title: undefined,
+            type: "countdown",
+          },
+        ],
+      );
+    });
+
+    it("returns addNewTimer", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      expect(
+        renderHook(() => useTimerList()).result.current.addNewTimer,
+      ).toBeInstanceOf(Function);
+    });
+
+    it("returns removeTimer", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      expect(
+        renderHook(() => useTimerList()).result.current.removeTimer,
+      ).toBeInstanceOf(Function);
+    });
+
+    it("returns updateTimer", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      expect(
+        renderHook(() => useTimerList()).result.current.updateTimer,
+      ).toBeInstanceOf(Function);
+    });
+
+    it("adds new timer when addNewTimer called", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      const hook = renderHook(() => useTimerList());
+      expect(hook.result.current.timerList).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+      ]);
+      hook.result.current.addNewTimer("stopwatch");
+      hook.rerender();
+      expect(hook.result.current.timerList).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "stopwatch",
+        },
+      ]);
+    });
+
+    it("removes timer when removeTimer called", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      const hook = renderHook(() => useTimerList());
+      expect(hook.result.current.timerList).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+      ]);
+      hook.result.current.removeTimer(hook.result.current.timerList[0].id);
+      hook.rerender();
+      expect(hook.result.current.timerList).toEqual([]);
+    });
+
+    it("updates timer when updateTimer called", () => {
+      vi.spyOn(Storage.prototype, "getItem").mockReturnValue(null);
+      getTimerFromUrl.mockReturnValue(null);
+      const hook = renderHook(() => useTimerList());
+      expect(hook.result.current.timerList).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: undefined,
+          title: undefined,
+          type: "countdown",
+        },
+      ]);
+      hook.result.current.updateTimer({
+        id: hook.result.current.timerList[0].id,
+        title: "new title",
+        timerValue: { hours: 0, minutes: 0, seconds: 0 },
+        type: "stopwatch" as TimerType,
+      });
+
+      hook.rerender();
+      expect(hook.result.current.timerList).toEqual([
+        {
+          id: expect.any(String),
+          timerValue: { hours: 0, minutes: 0, seconds: 0 },
+          title: "new title",
+          type: "stopwatch",
+        },
+      ]);
+    });
+  });
+});
