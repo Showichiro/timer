b3b2e6c689677f12919e5f213841a7218808db07
uchihori <show2305@gmail.com>
2023-11-19 01:42:12 +0900

    test refactor

--- src/utils/urlUtil.test.ts
+++ src/utils/urlUtil.test.ts
@@ -1,97 +1,68 @@
 import { getTimerFromUrl } from "./urlUtil";
 
 describe("getTimerFromUrl", () => {
-  const original = window.location;
-  afterEach(() => {
-    window.location = original;
-  });
-
   const url = "https://example.com";
 
   it("should return the timer value from the url (1)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/1`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/1`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 0, minutes: 1, seconds: 0 });
   });
 
   it("should return the timer value from the url (01)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/01`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/01`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 0, minutes: 1, seconds: 0 });
   });
 
   it("should return the timer value from the url (1:12)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/1:12`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/1:12`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 0, minutes: 1, seconds: 12 });
   });
 
   it("should return the timer value from the url (01:12)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/01:12`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/01:12`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 0, minutes: 1, seconds: 12 });
   });
 
   it("should return the timer value from the url (02:01:12)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/02:01:12`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/02:01:12`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 2, minutes: 1, seconds: 12 });
   });
 
   it("should return the timer value from the url (02:1:12)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/02:1:12`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/02:1:12`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual({ hours: 2, minutes: 1, seconds: 12 });
   });
 
   it("should return the timer value from the url (none)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: url,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual(null);
   });
 
   it("should return the timer value from the url (invalid value)", () => {
-    Object.defineProperty(window, "location", {
-      value: {
-        href: `${url}/abc`,
-      },
-      writable: true,
-    });
+    vi.spyOn(window, "location", "get").mockReturnValue({
+      href: `${url}/abc`,
+    } as Window["location"]);
     const timer = getTimerFromUrl();
     expect(timer).toStrictEqual(null);
   });
